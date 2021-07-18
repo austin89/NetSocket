aa20ccc06c611d40b8544abdf72a2b5c9be7e5d8
Austin C <acouey119@gmail.com>
2021-07-18 19:43:56 -0400

    streaming data from file

--- server/index.js
+++ server/index.js
@@ -1,89 +1,13 @@
 var net = require("net");
-const Stream = require("stream");
+var fs = require("fs");
 
 // creates the server
 var server = net.createServer();
-
-let sendData = [
-  "L1",
-  "R3",
-  "L3",
-  "R5",
-  "L7",
-  "R7",
-  "L8",
-  "R9",
-  "L6",
-  "R6",
-  "L5",
-  "R3",
-  "L3",
-  "R5",
-  "L7",
-  "R7",
-  "L5",
-  "R5",
-  "L6",
-  "R6",
-  "L1",
-  "R3",
-  "L3",
-  "R5",
-  "L7",
-  "R7",
-  "L9",
-  "R9",
-  "L6",
-  "R6",
-  "L1",
-  "R3",
-  "L3",
-  "R5",
-  "L7",
-  "R7",
-  "L2",
-  "R2",
-  "L6",
-  "R6",
-];
-
-const buffer = Buffer.from(sendData);
 
 // emitted when new client connects
 server.on("connection", function (socket) {
   console.log("Server on 'connection'");
 
-  //this property shows the number of characters currently buffered to be written. (Number of characters is approximately equal to the number of bytes to be written, but the buffer may contain strings, and the strings are lazily encoded, so the exact number of bytes is not known.)
-  //Users who experience large or growing bufferSize should attempt to "throttle" the data flows in their program with pause() and resume().
-
-  // console.log('Buffer size : ' + socket.bufferSize);
-
-  // console.log('---------server details -----------------');
-
-  // var address = server.address();
-  // var port = address.port;
-  // var family = address.family;
-  // var ipaddr = address.address;
-  // console.log('Server is listening at port ' + port);
-  // console.log('Server ip : ' + ipaddr);
-  // console.log('Server is IP4/IP6 : ' + family);
-
-  // var lport = socket.localPort;
-  // var laddr = socket.localAddress;
-  // console.log('Server is listening at LOCAL port ' + lport);
-  // console.log('Server LOCAL ip : ' + laddr);
-
-  // console.log('------------remote client info --------------');
-
-  // var rport = socket.remotePort;
-  // var raddr = socket.remoteAddress;
-  // var rfamily = socket.remoteFamily;
-
-  // console.log('REMOTE Socket is listening at port' + rport);
-  // console.log('REMOTE Socket ip :' + raddr);
-  // console.log('REMOTE Socket is IP4/IP6 : ' + rfamily);
-
-  // console.log('--------------------------------------------')
   //var no_of_connections =  server.getConnections(); // sychronous version
   server.getConnections(function (error, count) {
     console.log("Number of concurrent connections to the server : " + count);
@@ -162,29 +86,27 @@ server.on("connection", function (socket) {
     }
   });
 
-  // setTimeout(function(){
-  //   var isdestroyed = socket.destroyed;
-  //   console.log('Socket destroyed:' + isdestroyed);
-  //   socket.destroy();
-  // },12000000);
+  setTimeout(function () {
+    let stream = fs.createReadStream("./sensorData.txt");
+    stream.on("data", function (data) {
+      // let head = Buffer.from("OPEN");
+      // let buffer = Buffer.concat([head, data]);
+      // let bufStr = buffer.toString();
+      // console.log("bufStr: ", bufStr);
+      socket.write(data);
+    });
+  }, 1000);
 
-  function send() {
-    // const readable = new Stream.Readable();
-    // readable.pipe(process.stdout);
-    // sendData.forEach((item) => readable.push(item));
-    // // no more data
-    // readable.push(null);
-    //socket.write(buffer);
+  // function send() {
+  //   let data = sendData.pop();
+  //   console.log("sending data: ", data);
+  //   if (data == undefined) {
+  //     clearInterval(handle);
+  //     return;
+  //   } else socket.write(data);
+  // }
 
-    let data = sendData.pop();
-    console.log("sending data: ", data);
-    if (data == undefined) {
-      clearInterval(handle);
-      return;
-    } else socket.write(data);
-  }
-
-  let handle = setInterval(send, 500);
+  // let handle = setInterval(send, 500);
 });
 
 //emitted when server closes ...not emitted until all connections closes.
